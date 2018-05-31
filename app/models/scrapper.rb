0b63fa7c81a3abfded5361a2604454714e05473f
kenigbolo <expensivestephen@hotmail.com>
2018-06-01 02:40:07 +0300

    refactor scrapper model and remove unnecessary code

--- app/models/scrapper.rb
+++ app/models/scrapper.rb
@@ -10,23 +10,18 @@ class Scrapper < ApplicationRecord
   private
   def scrape_content
     page_contents = Nokogiri::HTML(open(web_url))
-    save_content(page_contents, %w(h1 h2 h3 a))
+    save_content(page_contents)
   end
 
-  def save_content(page_contents, html_tags)
-    html_tags.each do |html_tag|
+  def save_content(page_contents)
+    %w(h1 h2 h3 a).each do |html_tag|
       tags_list = page_contents.css(html_tag)
       filter_and_save(tags_list, html_tag) if tags_list.length >= 1
     end
   end
 
   def filter_and_save(tags_list, tag)
-    tag_values = tags_list.map { |tag| tag.text } unless anchor(tag)
-    tag_values = tags_list.map { |tag| tag['href'] } if anchor(tag)
+    tag_values = tags_list.map { |tag| tag.text || tag['href'] }
     tag_values.map { |value| ScrappedContent.create(scrapper_id: id, tag: tag, value: value)}
-  end
-
-  def anchor(tag)
-    tag == 'a'
   end
 end
